eee846f6e839dfd9a3cac39e70ec0ee9811ece78
manuelz <zangl.manuel@gmail.com>
2025-12-30 13:17:30 +0100

    Initial Commit

---
+++ ExpenseTrackerTests/ExpenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerTests.swift
+//  ExpenseTrackerTests
+//
+//  Created by Manuel Zangl on 30.12.25.
+//
+
+import Testing
+@testable import ExpenseTracker
+
+struct ExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
